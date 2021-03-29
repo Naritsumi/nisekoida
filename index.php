a02f6c2216696a30ed10e7493afad411155a3263
naritsumi <haridian6bnaza09@gmail.com>
2021-03-29 21:17:05 +0100

    Web update

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>File Upload and Download</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <form action="index.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="myfile"><br>
+                    <button type="submit" name="save">Upload</button>
+
+                </form>
+            </div>
+
+    </body>
+</html>
